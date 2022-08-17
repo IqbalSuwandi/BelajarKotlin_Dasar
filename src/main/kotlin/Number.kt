fd6523754d51da7d5dd2cc0bed5cd472d47698b0
Iqbal Suwandi <99460725+IqbalSuwandi@users.noreply.github.com>
2022-08-17 19:59:28 +0700

    version2

---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var age : Byte = 30
+    var height : Int = 40
+    var distance : Short = 200
+    var balance : Long = 9000000000000L
+
+    println(age)
+    println(height)
+    println(distance)
+    println(balance)
+}
